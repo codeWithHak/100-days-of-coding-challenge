4ffb760a64b9e3a007b63f9b89931c50df9efb2e
Huzair Ahmed Khan <124548356+codeWithHak@users.noreply.github.com>
2024-03-14 07:48:20 +0500

    Update index.ts

--- Day-04/Question-16/index.ts
+++ Day-04/Question-16/index.ts
@@ -6,15 +6,3 @@
 // - Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list.
 
 // - Print a new set of invitation messages, one for each person in your list.
-
-let guestList:string[] = ["Babar Azam","Shadab Khan","Saim Ayub"];
-guestList.pop()
-guestList.push("Naseem Shah")
-
-for (const guest of guestList) {
-    console.log(`Dear ${guest}, I would love to have you for dinner tonight.`);
-    
-}
-
-
- export{} // used export just to remove red squiggly line
