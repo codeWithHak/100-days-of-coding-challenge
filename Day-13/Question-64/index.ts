75c6910286563ee2a5e4f9ff7a27e70d5df3e444
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-03-29 04:27:09 +0500

    intialize day-13

---
+++ Day-13/Question-64/index.ts
@@ -0,0 +1,5 @@
+/*
+
+Q64 - Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
+
+*/
